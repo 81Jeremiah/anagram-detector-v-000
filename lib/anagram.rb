bea85df5dc6c67f4d377858280a4f6c1d9a9d199
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-07 05:01:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     #anagram_array =[]
     #@words = words
     words.select do |word|
-      if word.split("").sort == @word.split("").sort
+       word.split("").sort == @word.split("").sort
       #anagram_array  << word
       end
       #binding.pry
