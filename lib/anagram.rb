795cf0f67d1538c3f6eaed99a92e2609fb812ef8
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-07 04:34:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    @words = words
+    @words.each do |word|
+      if word.split("").sort == @word.split("").sort
+        word
+      else
+        binding.pry
+        @words = []
+        @words
+      end
+    end
+  end
+   #takes the letter in
+
+end
